a4bd531ca6169283233a6029091469cd2a8b0466
Vr3n <vickyspatel@gmail.com>
2022-09-07 19:40:43 +0530

    Cleanup

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from .database import engine
 from .routers import post, user, auth, voting
 
 app = FastAPI()
-models.Base.metadata.create_all(bind=engine)
+# models.Base.metadata.create_all(bind=engine)
 
 
 @app.get("/")
